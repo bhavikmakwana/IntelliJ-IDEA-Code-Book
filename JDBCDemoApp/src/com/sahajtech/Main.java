9d3dd05788a03feb75f69b6a45052385d0e5b44d
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2017-09-18 13:00:48 +0530

    final change for get all tables name and table info

--- JDBCDemoApp/src/com/sahajtech/Main.java
+++ JDBCDemoApp/src/com/sahajtech/Main.java
@@ -16,7 +16,7 @@ public class Main {
             System.out.println("Connection successfully");
             DatabaseMetaData databaseMetaData = connection.getMetaData();
             ResultSet tables = databaseMetaData.getTables(null, null, null, new String[]{"TABLE"});
-           /* while (tables.next()) {
+            while (tables.next()) {
                 String tablename = tables.getString("TABLE_NAME");
                 System.out.println("----------------------------------------------------------------" + tablename + "----------------------------------------------------------------");
                 ResultSet resultSet = databaseMetaData.getColumns(null, null, tablename, null);
@@ -31,7 +31,7 @@ public class Main {
                             + "\tNullable - "+ resultSet.getInt("NULLABLE"));
                     i++;
                 }
-            }*/
+            }
             connection.close();
         } catch (ClassNotFoundException e) {
             System.out.println("class not found exception -" + e.getMessage());
@@ -43,42 +43,4 @@ public class Main {
         }
     }
 
-    public static Connection getPostgresConnetion(String connectionString, String username ,String password)throws Exception{
-        Class.forName("org.postgresql.Driver");
-        Connection connection  = DriverManager.getConnection(connectionString,username,password);
-        System.out.println("Connection Establish successfully");
-        return connection;
-    }
-    public static ResultSet getAllTablesName(Connection connection){
-        DatabaseMetaData databaseMetaData = null;
-        try {
-            databaseMetaData = connection.getMetaData();
-            //noinspection UnnecessaryLocalVariable
-            ResultSet tables = databaseMetaData.getTables(null, null, null, new String[]{"TABLE"});
-            return tables;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    public static void getAllTableInfo(String tableName,Connection connection) throws SQLException {
-        DatabaseMetaData databaseMetaData = connection.getMetaData();
-        ResultSet tables = databaseMetaData.getTables(null, null, null, new String[]{"TABLE"});
-        while (tables.next()) {
-            String tablename = tables.getString("TABLE_NAME");
-            System.out.println("----------------------------------------------------------------" + tableName + "----------------------------------------------------------------");
-            ResultSet resultSet = databaseMetaData.getColumns(null, null, tableName, null);
-            int i = 1;
-            while (resultSet.next()) {
-                System.out.println(i +
-                        " Table Schema - "+  resultSet.getString("TABLE_SCHEM")
-                        + "\tTable Name - "+ resultSet.getString("TABLE_NAME")
-                        + "\tcolumn Name - "+ resultSet.getString("COLUMN_NAME")
-                        + "\tType Name - "+ resultSet.getString("TYPE_NAME")
-                        + "\tColumn Size - "+ resultSet.getInt("COLUMN_SIZE")
-                        + "\tNullable - "+ resultSet.getInt("NULLABLE"));
-                i++;
-            }
-        }
-    }
 }
